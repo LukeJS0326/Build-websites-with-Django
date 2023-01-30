d12cea84f9ada71a2b019c1ced8a6aef027d53a5
LukeJS0326 <lukejs0326@gmail.com>
2023-01-31 06:49:03 +0900

    Printing Hello World! using Django App

--- myproject/urls.py
+++ myproject/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import myapp.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',myapp.views.hello_world, name='hello_world')
 ]
